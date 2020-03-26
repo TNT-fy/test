8a276be6f5332629cb11d91c2418bbedac9cb5bb
fuyueyu <fuyueyu@DESKTOP-FMJE8T2>
2020-03-26 09:23:27 +0800

    1

--- test/src/test/test1.java
+++ test/src/test/test1.java
@@ -2,9 +2,5 @@ package test;
 
 public class test1 {
 public static void main(String[] args) {
-	System.err.println("ÎÒ½Ğ·üÔ¾Óî");
-	System.err.println("2222");
-	System.err.println("2222");
-	System.err.println("2222");
-}
+	System.out.println("ÎÒ½Ğ·üÔ¾Óî");}
 }
